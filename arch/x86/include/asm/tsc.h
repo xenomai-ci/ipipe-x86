1542528e0380639fd202e91695f3dae842428051
Philippe Gerum <rpm@xenomai.org>
2018-02-06 12:12:19 +0100

    x86: ipipe: notify about GTOD update
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- arch/x86/include/asm/tsc.h
+++ arch/x86/include/asm/tsc.h
@@ -15,6 +15,9 @@
  */
 typedef unsigned long long cycles_t;
 
+extern struct clocksource clocksource_tsc;
+#define __ipipe_hostrt_clock	clocksource_tsc
+
 extern unsigned int cpu_khz;
 extern unsigned int tsc_khz;
 
