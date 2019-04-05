1f27c18ec4bd28d5febf8f49e90e26f32786fbb2
Jan Kiszka <jan.kiszka@siemens.com>
2019-04-05 20:04:56 +0200

    x86/ipipe: kvm: Add missing hard irq re-enabling calls
    Those got logically lost during the port to 4.14
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -7179,6 +7179,7 @@ static int vcpu_enter_guest(struct kvm_vcpu *vcpu)
 	    || need_resched() || signal_pending(current)) {
 		vcpu->mode = OUTSIDE_GUEST_MODE;
 		smp_wmb();
+		hard_cond_local_irq_enable();
 		local_irq_enable();
 		preempt_enable();
 		vcpu->srcu_idx = srcu_read_lock(&vcpu->kvm->srcu);
@@ -7247,6 +7248,7 @@ static int vcpu_enter_guest(struct kvm_vcpu *vcpu)
 
 	guest_exit_irqoff();
 
+	hard_cond_local_irq_enable();
 	local_irq_enable();
 	preempt_enable();
 
